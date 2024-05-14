fb78220dd2e3fd6ad988fb4cf60c18d69c049e41
leonardeganow <leonard@eganow.com>
2024-05-14 09:57:15 +0000

    added login functionality

---
+++ src/app/constants/index.js
@@ -0,0 +1,4 @@
+export const PASSWORD_REGEXP =
+  /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* ).{8,16}$/;
+
+ export const BASE_URL = 'http://localhost:4000'
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,18 @@
 "use client";
-import Link from "next/link";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import clsx from "clsx";
+import { PASSWORD_REGEXP } from "./constants";
+import { loginUser } from "./api/AuthApi";
+import { useRouter } from "next/navigation";
+import { Circles } from "react-loader-spinner";
+import { useState } from "react";
+import { FaFire } from "react-icons/fa";
 
 export default function Home() {
+  const [errors, setErrors] = useState();
+  const router = useRouter();
   const defaultFormValues = {
     email: "",
     password: "",
@@ -13,17 +20,30 @@ export default function Home() {
 
   const validationSchema = yup.object({
     email: yup.string().email().required("Email is required"),
-    password: yup.string().required("Password is required"),
+    password: yup
+      .string()
+      .required("Password is required")
+      .matches(PASSWORD_REGEXP, "Wrong password format"),
   });
 
-  const { register, handleSubmit, formState } = useForm({
+  const { register, handleSubmit, formState, reset } = useForm({
     mode: "onChange",
     resolver: yupResolver(validationSchema),
     defaultValues: defaultFormValues,
   });
 
   const onSubmit = async (values) => {
-    console.log(values);
+    try {
+      const response = await loginUser(values);
+      if (response?.data?.token) {
+        router.push("/home");
+        return;
+      }
+      reset(values);
+    } catch (error) {
+      console.log(error);
+      setErrors(error.response.data.error);
+    }
   };
 
   return (
@@ -31,9 +51,18 @@ export default function Home() {
       <div className="border-2 border-green-700 p-5 rounded shadow-lg">
         <form
           onSubmit={handleSubmit(onSubmit)}
-          className="flex flex-col text-center gap-y-8 items-center"
+          className="flex flex-col text-center gap-y-6 items-center"
         >
-          <h1 className="text-gray-500">Feedback</h1>
+          <div>
+
+          <h1 className="text-gray-500 mb-1">Feedback</h1>
+          {errors && (
+            <div className="flex items-center gap-2 bg-red-500 text-xs p-1 text-red-100 border-4 border-red-200 rounded">
+              <FaFire size={20}/>
+              {errors}
+            </div>
+          )}
+          </div>
           <div>
             <input
               type="text"
@@ -48,7 +77,7 @@ export default function Home() {
               {...register("email")}
             />
             {formState.errors.email && (
-              <p className="text-red-500 text-left text-xs py-1">
+              <p className="text-red-500 text-left text-xs py-1 ">
                 {formState.errors.email.message}
               </p>
             )}
@@ -68,16 +97,29 @@ export default function Home() {
               {...register("password")}
             />
             {formState.errors.password && (
-              <p className="text-red-500 text-left text-xs py-1">
+              <p className="text-red-500 text-left text-xs py-1 ">
                 {formState.errors.password.message}
               </p>
             )}
           </div>
           <button
             type="submit"
+            disabled={formState.isSubmitting}
             className="bg-green-700 px-5 py-1 text-gray-200 rounded active:bg-green-900"
           >
-            log in
+            {formState.isSubmitting ? (
+              <Circles
+                height="20"
+                width="20"
+                color="white"
+                ariaLabel="circles-loading"
+                wrapperStyle={{}}
+                wrapperClass=""
+                visible={true}
+              />
+            ) : (
+              "log in"
+            )}
           </button>
         </form>
       </div>
